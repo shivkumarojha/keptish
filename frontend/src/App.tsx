ce34115400341b2694ec08ec6d0f47da0e8b88c2
shivkumarojha <ojhashivkumar88@gmail.com>
2024-11-05 21:26:40 +0530

    init frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,18 @@
+function App() {
+  return (
+    <div className="">
+      <div className="flex justify-center pt-5">
+        <div className="text-4xl font-extrabold text-gray-600">
+          <h1>
+            {" "}
+            Keptish: <span className="text-amber-600">
+              Ultrafast Notes
+            </span>{" "}
+          </h1>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
