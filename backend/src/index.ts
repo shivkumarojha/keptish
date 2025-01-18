a8043e19a4687565ff7241184744976422ccab82
shivkumarojha <ojhashivkumar88@gmail.com>
2025-01-18 14:19:20 +0530

    prisma added

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,12 +2,18 @@ import 'dotenv/config'
 import express from "express"
 
 
+// Initializing express app
 const app = express()
 
+
+// Health route
 app.get("/health", (req, res) => {
     res.status(200).json({
         message: "Ok"
     })
 })
+
+
+
 
 app.listen(process.env.PORT)
